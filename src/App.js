635ccfb898865941b567e1e702fa4dc340cb2f8f
EstrellaDionis <EstrellaDionis@gmail.com>
2021-11-30 17:53:27 -0500

    created core jsx with my information and one follower

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+'./App.css';
 
 class App extends React.Component {
   render() {
     return(<div>
-      Github Card
-    </div>);
+      <h1>Github Info</h1>
+      <form>
+        <input placeholder='Github Handle'/>
+        <button>Search</button>
+      </form>
+      <div id='UserCard'>
+        <img src="https://avatars.githubusercontent.com/u/87622513?v=4" />
+        <a target="_blank" rel='noreferrer' href="https://github.com/EstrellaDionis"><h3>Dionis Estrella</h3></a>
+        <p>Estrella Dionis</p>
+        <p>Total Repos: 35</p>
+        <p>Total Followers: 2</p>
+      </div>
+
+      <div id='followers'>
+          <div className='follower'>
+            <img src="https://avatars.githubusercontent.com/u/6520868?v=4" />
+            <a target="_blank" rel='noreferrer' href="https://github.com/MarkRivera"><p>MarkRivera</p></a>
+          </div>
+      </div>
+    </div>
+    );
   }
 }
 
